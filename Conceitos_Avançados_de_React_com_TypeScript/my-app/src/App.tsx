717bc0c0a7d7475585673028d5a683db67ce3d71
monique.elisa.d.beco <monique.elisa.d.beco@accenture.com>
2023-11-27 20:40:51 -0300

    Intro Context API

--- Conceitos_Avançados_de_React_com_TypeScript/my-app/src/App.tsx
+++ Conceitos_Avançados_de_React_com_TypeScript/my-app/src/App.tsx
@@ -4,19 +4,38 @@ import { Layout } from './components/Layout'
 import Conta from './pages/Conta';
 import Home from "./pages/Home";
 import ContaInfo from "./pages/ContaInfo";
+import { createContext } from 'react';
+
+interface IAppContext {
+  user: string
+}
+
+export const AppContext = createContext({} as IAppContext)
+
+const AppContextProvider = ({ children }: any) => {
+  const user = "MB"
+
+  return (
+    <AppContext.Provider value={{ user }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <ChakraProvider>
-        <Layout>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/conta/:id' element={<Conta />} />
-            <Route path='/containfo' element={<ContaInfo />} />
-          </Routes>
-        </Layout>
-      </ChakraProvider>
+      <AppContextProvider>
+        <ChakraProvider>
+          <Layout>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/conta/:id' element={<Conta />} />
+              <Route path='/containfo' element={<ContaInfo />} />
+            </Routes>
+          </Layout>
+        </ChakraProvider>
+      </AppContextProvider>
     </BrowserRouter>
   );
 }
--- Conceitos_Avançados_de_React_com_TypeScript/my-app/src/components/Header/Header.tsx
+++ Conceitos_Avançados_de_React_com_TypeScript/my-app/src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
-import './Header.css'
-import { ChakraProvider, Heading, Center, Box } from '@chakra-ui/react'
+import { ChakraProvider, Heading, Center, Box, Button } from '@chakra-ui/react';
+import { useContext } from 'react';
+import { AppContext } from '../../App';
 
 export const Header = () => {
+    const context = useContext(AppContext)
+    console.log(context)
+    
     return (
         <ChakraProvider>
             <Box minHeight='10vh' backgroundColor='#dca8f9' padding='25px'>
